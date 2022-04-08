647e05689fa77a0fa7c353ab96d303ac4dbc71f3
Ethan <n.ethan.naugle@gmail.com>
2022-04-07 23:23:02 -0400

    adapting for different screens

--- users/static/base.js
+++ users/static/base.js
@@ -1,3 +1,7 @@
 let imagebtn = document.getElementsByClassName("clearablefileinput");
 
 imagebtn.innerText = "img";
+var siteWidth = 1280;
+var scale = screen.width /siteWidth;
+
+document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');

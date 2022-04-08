3292e493eb09376db31acbfa0807e8152234c828
37h4nn4ug13 <nnaugle1@com1980.eecs.utk.edu>
2022-04-08 15:04:57 -0400

    Trying to fix static file issues

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -122,7 +122,9 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'users/static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static/'),
+)
 
 STATIC_URL = '/static/'
 

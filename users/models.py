8a157f547122d6ceccf520028f58f12143de81bb
Ethan <n.ethan.naugle@gmail.com>
2022-04-09 13:54:25 -0400

    Resize is successful

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image, ExifTags
 
 # Create your models here.
 
@@ -12,3 +13,14 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username + "'s profile"
         #return "user number " 
+
+    def save(self):
+        super().save()
+        width = 300
+        img = Image.open(self.image.path)
+
+        if (img.width > width):
+            img.thumbnail((width, int(img.width/img.height*width)))
+            
+            img.save(self.image.path)
+

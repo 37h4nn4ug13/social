8ab28000215fc65d1d0b692b4d8443dd815611d2
Ethan <n.ethan.naugle@gmail.com>
2022-04-08 23:31:55 -0400

    First commit

--- users/views.py
+++ users/views.py
@@ -1,9 +1,13 @@
+import imp
+from urllib import request
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import DetailView, ListView
+from django.views.generic.edit import FormMixin
 from django.contrib import messages
+from django.urls import reverse
 
-from .forms import UserRegisterForm, UserLoginForm
+from .forms import ProfileUpdateForm, UserRegisterForm, UserLoginForm, UserUpdateForm, ProfileUpdateForm
 from .models import Profile
 from posts.models import Post
 from django.contrib.auth import logout, login, authenticate
@@ -34,15 +38,32 @@ def register(request):
     return render(request, 'users/register.html', context)
 
 
-class PersonalProfile(DetailView):
+class PersonalProfile(FormMixin, DetailView):
     model = Profile
     template_name = 'users/profile.html'
     context_object_name = 'profile'
+    form_class = UserUpdateForm
+
+    def get_success_url(self):
+        return reverse('profile')
+
+    def post(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return HttpResponseForbidden()
+        self.object = self.get_object()
+        form = self.get_form()
+        if form.is_valid():
+            request.user.email = form.cleaned_data.get('email')
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
 
     def get_object(self, **kwargs):
         return self.request.user
 
     def get_context_data(self, *, object_list=None, args=None, **kwargs):
+
+
         context = super(PersonalProfile, self).get_context_data(**kwargs)
         context['posts'] = Post.objects.all()
         return context

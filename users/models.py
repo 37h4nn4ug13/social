15eeff60a6df78e9b41d23bf1703487fbaa540e8
Ethan <n.ethan.naugle@gmail.com>
2022-04-09 21:36:23 -0400

    Finally the rotation is correct

--- users/models.py
+++ users/models.py
@@ -1,7 +1,12 @@
+import imp
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image, ExifTags
-
+from .utils import rotate_image
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+import os
+from socialmedia.settings import BASE_DIR
 # Create your models here.
 
 class Profile(models.Model):
@@ -20,7 +25,17 @@ class Profile(models.Model):
         img = Image.open(self.image.path)
 
         if (img.width > width):
-            img.thumbnail((width, int(img.width/img.height*width)))
+            if (img.width < img.height):
+                img.thumbnail((int(img.width/img.height*width), width))
+            else:
+                img.thumbnail((width, int(img.width/img.height*width)))
             
             img.save(self.image.path)
+@receiver(post_save, sender=Profile, dispatch_uid="update_image_profile")
+def update_image(sender, instance, **kwargs):
+    if instance.image:
+        fullpath = os.path.join(BASE_DIR, instance.image.path)
+        print(BASE_DIR)
+        print(fullpath)
+        rotate_image(fullpath)
 
